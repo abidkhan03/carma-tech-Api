0d4e622e6e306ee4378319b41128b0c8dfc435db
abidkhan03 <abidkhan42935@gmail.com>
2023-09-25 17:27:33 +0500

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,8 +5,8 @@ import {
   UseGuards,
   Get,
   Request,
-  ConflictException,
   BadRequestException,
+  ConflictException,
 } from '@nestjs/common';
 import { ApiResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
@@ -15,22 +15,27 @@ import { SigninDto } from '@modules/auth/dto/signin.dto';
 import { SignupDto } from '@modules/auth/dto/signup.dto';
 import { UsersService } from '@modules/user/user.service';
 import { IRequest } from '@modules/user/user.interface';
-import { Lambda } from 'aws-sdk';
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda';
 import { Logger } from '@aws-lambda-powertools/logger';
+import { Lambda } from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('api/auth')
 @ApiTags('authentication')
 export class AuthController {
-  private lambda: Lambda;
+  private lambdaClient: Lambda;
   private readonly logger = new Logger();
   constructor(
     private readonly authService: AuthService,
     private readonly userService: UsersService,
+    private readonly configService: ConfigService
   ) {
-    this.lambda = new Lambda({
+
+    this.lambdaClient = new Lambda({
       region: 'us-east-2',
     });
   }
+
 
   @Post('signin')
   @ApiResponse({ status: 201, description: 'Successful Login' })
@@ -51,46 +56,45 @@ export class AuthController {
       if (existingUser.email === signupDto.email && existingUser.phone === signupDto.phone) {
         this.logger.error(`User with provided email and phone number already exists: ${JSON.stringify(existingUser)}`);
         throw new ConflictException('User with provided email and phone number already exist');
+
       } else if (existingUser.email === signupDto.email) {
         this.logger.error(`User with provided email and phone number already exist: ${JSON.stringify(existingUser)}`);
         throw new ConflictException('User with provided email already exist');
+
       } else {
         this.logger.error(`User with provided phone number already exist: ${JSON.stringify(existingUser)}`);
         throw new ConflictException('User with provided phone number already exist');
       }
     }
     try {
-
       const lambdaPayload = {
         queryStringParameters: {}
       };
+
       if (signupDto.email) {
         lambdaPayload.queryStringParameters['email'] = signupDto.email;
       } else if (signupDto.phone) {
         lambdaPayload.queryStringParameters['phone'] = signupDto.phone;
       };
       const lambdaParams = {
-        FunctionName: 'UserManagementStack-CreateUserLambda0154A2EB-5ufMqT4E5ntw',
+        FunctionName: this.configService.get('COGNITO_USER_MGMT_LAMBDA'),
         Payload: JSON.stringify(lambdaPayload),
       };
       this.logger.info(`lambda function params: ${JSON.stringify(lambdaParams)}`)
 
-      const lambdaResponse = await this.lambda.invoke(lambdaParams).promise();
+      const lambdaResponse = await this.lambdaClient.invoke(lambdaParams).promise();
       this.logger.info(`lambda response payload: ${JSON.stringify(lambdaResponse)}`);
-      this.logger.info(`lambda logs result: ${lambdaResponse.LogResult}`);
       const parsedPayload = JSON.parse(lambdaResponse.Payload as string);
       this.logger.info(`parsed payload: ${JSON.stringify(parsedPayload)}`);
-      const cognitoUser = parsedPayload.body;
-      this.logger.info(`cognito user: ${JSON.stringify(cognitoUser)}`);
 
       if (parsedPayload.statusCode === 400
         && parsedPayload.body.includes(
           'User with email already exists')) {
-        throw new ConflictException('User with provided email already exists in Cognito.');
+        throw new ConflictException('User with provided email already exist in Cognito');
       } else if (parsedPayload.statusCode === 400
         && parsedPayload.body.includes(
           'User with phone number already exists')) {
-        throw new ConflictException('User with provided phone number already exists in Cognito.');
+        throw new ConflictException('User with provided phone number already exist in Cognito');
       }
       // Handle any errors from the lambda function
       if (lambdaResponse.FunctionError) {
