40f6051613d95f439902703c42fb6aa38c601515
abidkhan03 <abidkhan42935@gmail.com>
2023-09-25 12:28:26 +0500

    Added Config and Http Service tests

--- src/modules/aws/sns/sns.controller.spec.ts
+++ src/modules/aws/sns/sns.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SnsController } from './sns.controller';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 
 describe('SnsController', () => {
   let controller: SnsController;
@@ -7,6 +9,20 @@ describe('SnsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SnsController],
+      providers: [
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(), // mock the methods you use from HttpService
+          },
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn(), // mock the methods you use from ConfigService
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<SnsController>(SnsController);
