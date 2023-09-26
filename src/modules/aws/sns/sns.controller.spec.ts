e18c78a2e707e3322d14b3a4510c0200837d1531
abidkhan03 <abidkhan42935@gmail.com>
2023-09-26 15:49:31 +0500

    Added the Logger mock test

--- src/modules/aws/sns/sns.controller.spec.ts
+++ src/modules/aws/sns/sns.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SnsController } from './sns.controller';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@aws-lambda-powertools/logger';
 
 describe('SnsController', () => {
   let controller: SnsController;
@@ -22,6 +23,12 @@ describe('SnsController', () => {
             get: jest.fn(), // mock the methods you use from ConfigService
           },
         },
+        {
+          provide: Logger,
+          useValue: {
+            debug: jest.fn(),
+          }
+        }
       ],
     }).compile();
 
