739f4402f0e6df3cd9f1c08ca1d07bd7c5b34f38
abidkhan03 <abidkhan42935@gmail.com>
2023-10-03 14:11:22 +0500

    Update new version

--- src/modules/aws/sns/sns.controller.ts
+++ src/modules/aws/sns/sns.controller.ts
@@ -20,98 +20,149 @@ export class SnsController {
             region: 'us-east-2',
         });
     }
-    346866
+
+
     @Post()
     async processSNSNotification(@Body() snsMessage: any): Promise<string> {
 
-        this.logger.info(`sns Message: ${JSON.stringify(snsMessage)}`);
-        if (snsMessage && Array.isArray(snsMessage.data)) {
-            this.logger.info(`SNS Message Data: ${JSON.stringify(snsMessage.data)}`);
-            
-        } else {
-            this.logger.error(`Unexpected SNS Message format: ${JSON.stringify(snsMessage)}`);
-        }
-        // this.logger.info(`SNS Message Data: ${JSON.stringify(snsMessage.data)}`);
+        this.logger.info(`Received SNS Message: ${JSON.stringify(snsMessage)}`);
 
-        const decodeData = new TextDecoder('utf-8').decode(new Uint8Array(snsMessage.data));
-        this.logger.info(`Decoded Data: ${decodeData}`);
+        this.logger.info(`Received SNS Data: ${snsMessage.data}`);
+        this.logger.info(`Received SNS Message Data: ${snsMessage.Message.data}`);
 
-        this.logger.info(`Decoded Data JSON: ${JSON.stringify(decodeData)}`);
-
-        const messageString = Buffer.from(snsMessage.data);
-        this.logger.info(`Converted json to buffer: ${JSON.stringify(messageString)}`);
-        const bufferString = messageString.toString('utf8');
-        this.logger.info(`Converted Buffer to String: ${bufferString}`);
-        this.logger.info(`buffer string--> ${JSON.stringify(bufferString)}`);
-
-        let parsedSnsMessage: any;
-
-        // Check if the message is a Buffer
+        // Ensure the message is in Buffer format
         if (snsMessage && snsMessage.type === 'Buffer' && Array.isArray(snsMessage.data)) {
             try {
                 // Convert the buffer to a string
                 const messageString = Buffer.from(snsMessage.data).toString('utf8');
                 this.logger.info(`Converted Message String: ${messageString}`);
 
-                // Try to parse the string to a JSON object
+                // Parse the string to a JSON object
                 const parsedSnsMessage = JSON.parse(messageString);
-                this.logger.info(`Parsed sns Message: ${JSON.stringify(parsedSnsMessage)}`);
+                this.logger.info(`Parsed SNS Message: ${JSON.stringify(parsedSnsMessage)}`);
 
-                // Additional handling code...
+                // Check if it's a SubscriptionConfirmation message
+                if (parsedSnsMessage.Type === 'SubscriptionConfirmation') {
+                    // Ensure the SubscribeURL is present
+                    if (!parsedSnsMessage.SubscribeURL) {
+                        this.logger.error(`SubscriptionConfirmation missing SubscribeURL: ${JSON.stringify(parsedSnsMessage)}}`);
+                        return "Error: SubscriptionConfirmation missing SubscribeURL";
+                    }
+
+                    // Confirm the subscription by visiting the SubscribeURL.
+                    try {
+                        const response = await lastValueFrom(this.httpService.get(parsedSnsMessage.SubscribeURL));
+                        this.logger.info(`Confirmed subscription with response: ${JSON.stringify(response)}`);
+                        return "Subscription successful";
+                    } catch (error) {
+                        this.logger.error(`Error confirming subscription: ${error.message}`);
+                        return "Error confirming subscription";
+                    }
+                } else {
+                    // Handle other message types as per your use case
+                    this.logger.warn(`Received non-subscription message type: ${parsedSnsMessage.Type}`);
+                }
             } catch (err) {
                 this.logger.error(`Error processing message: ${err.message}`);
                 return 'Error processing message';
             }
         } else {
             this.logger.warn('SNS Message is not in expected Buffer format.');
-            // Handle non-buffer message or throw an error as per your use case
         }
 
-        if (snsMessage.Message && snsMessage.Message.Type === 'SubscriptionConfirmation') {
-
-
-            if (parsedSnsMessage.Type === 'SubscriptionConfirmation') {
-                if (!snsMessage.SubscribeURL) {
-                    this.logger.error(`SubscriptionConfirmation missing SubscribeURL: ${JSON.stringify(snsMessage)}}`);
-                    return "Error: SubscriptionConfirmation missing SubscribeURL";
-                }
-                // Handle SNS subscription URL callback
-                // This URL should be fetched and visited to confirm the subscription.
-                const confirmationUrl = snsMessage.SubscribeURL;
-                this.logger.info(`Confirmation URL: ${confirmationUrl}`);
-                // Make an HTTP GET request to the provided URL to confirm the subscription.
-
-                try {
-                    const response = lastValueFrom(this.httpService.get(confirmationUrl));
-                    this.logger.info(`Confirmed subscription with response: ${JSON.stringify(response)}`);
-                    return "Subscription successful";
-
-                } catch (error) {
-                    this.logger.error("Error confirming subscription: ", error.message);
-                    return "Error confirming subscription2";
-                }
-
-            } else if (snsMessage.Type === 'Notification') {
-                if (!snsMessage.Message || !snsMessage.Status) {
-                    this.logger.error("Notification missing Message or Status");
-                    return "Error: Notification missing Message or Status";
-                }
-                if (snsMessage.Status === 'COMPLETED') {
-                    // Handle completed Lambda task
-                    // Store the result, notify a user, etc.
-                    console.log('Lambda task completed successfully.');
-
-                    const message = snsMessage.Message;
-                    this.logger.info(`message: ${JSON.stringify(message)}`);
-
-                }
-
-            }
-            // }
-
-            return 'OK';
-        }
+        return 'OK';
     }
+
+    // @Post()
+    // async processSNSNotification(@Body() snsMessage: any): Promise<string> {
+
+    //     this.logger.info(`sns Message: ${JSON.stringify(snsMessage)}`);
+    //     if (snsMessage && Array.isArray(snsMessage.data)) {
+    //         this.logger.info(`SNS Message Data: ${JSON.stringify(snsMessage.data)}`);
+
+    //     } else {
+    //         this.logger.error(`Unexpected SNS Message format: ${JSON.stringify(snsMessage)}`);
+    //     }
+
+    //     const decodeData = new TextDecoder('utf-8').decode(new Uint8Array(snsMessage.data));
+    //     this.logger.info(`Decoded Data: ${decodeData}`);
+
+    //     this.logger.info(`Decoded Data JSON: ${JSON.stringify(decodeData)}`);
+
+    //     const messageString = Buffer.from(snsMessage.data);
+    //     this.logger.info(`Converted json to buffer: ${JSON.stringify(messageString)}`);
+    //     const bufferString = messageString.toString('utf8');
+    //     this.logger.info(`Converted Buffer to String: ${bufferString}`);
+    //     this.logger.info(`buffer string--> ${JSON.stringify(bufferString)}`);
+
+    //     let parsedSnsMessage: any;
+
+    //     // Check if the message is a Buffer
+    //     if (snsMessage && snsMessage.type === 'Buffer' && Array.isArray(snsMessage.data)) {
+    //         try {
+    //             // Convert the buffer to a string
+    //             const messageString = Buffer.from(snsMessage.data).toString('utf8');
+    //             this.logger.info(`Converted Message String: ${messageString}`);
+
+    //             // Try to parse the string to a JSON object
+    //             const parsedSnsMessage = JSON.parse(messageString);
+    //             this.logger.info(`Parsed sns Message: ${JSON.stringify(parsedSnsMessage)}`);
+
+    //             // Additional handling code...
+    //         } catch (err) {
+    //             this.logger.error(`Error processing message: ${err.message}`);
+    //             return 'Error processing message';
+    //         }
+    //     } else {
+    //         this.logger.warn('SNS Message is not in expected Buffer format.');
+    //         // Handle non-buffer message or throw an error as per your use case
+    //     }
+
+    //     if (snsMessage.Message && snsMessage.Message.Type === 'SubscriptionConfirmation') {
+
+
+    //         if (parsedSnsMessage.Type === 'SubscriptionConfirmation') {
+    //             if (!snsMessage.SubscribeURL) {
+    //                 this.logger.error(`SubscriptionConfirmation missing SubscribeURL: ${JSON.stringify(snsMessage)}}`);
+    //                 return "Error: SubscriptionConfirmation missing SubscribeURL";
+    //             }
+    //             // Handle SNS subscription URL callback
+    //             // This URL should be fetched and visited to confirm the subscription.
+    //             const confirmationUrl = snsMessage.SubscribeURL;
+    //             this.logger.info(`Confirmation URL: ${confirmationUrl}`);
+    //             // Make an HTTP GET request to the provided URL to confirm the subscription.
+
+    //             try {
+    //                 const response = lastValueFrom(this.httpService.get(confirmationUrl));
+    //                 this.logger.info(`Confirmed subscription with response: ${JSON.stringify(response)}`);
+    //                 return "Subscription successful";
+
+    //             } catch (error) {
+    //                 this.logger.error("Error confirming subscription: ", error.message);
+    //                 return "Error confirming subscription2";
+    //             }
+
+    //         } else if (snsMessage.Type === 'Notification') {
+    //             if (!snsMessage.Message || !snsMessage.Status) {
+    //                 this.logger.error("Notification missing Message or Status");
+    //                 return "Error: Notification missing Message or Status";
+    //             }
+    //             if (snsMessage.Status === 'COMPLETED') {
+    //                 // Handle completed Lambda task
+    //                 // Store the result, notify a user, etc.
+    //                 console.log('Lambda task completed successfully.');
+
+    //                 const message = snsMessage.Message;
+    //                 this.logger.info(`message: ${JSON.stringify(message)}`);
+
+    //             }
+
+    //         }
+    //         // }
+
+    //         return 'OK';
+    //     }
+    // }
 
     private fieldsForSignature(type: string): string[] {
         if (type === 'SubscriptionConfirmation') {
