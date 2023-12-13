a13aea2ba210004c6ce4a9d3b6ec7d1f5ec3b1a7
abidkhan03 <abidkhan42935@gmail.com>
2023-12-13 16:16:01 +0500

    Update getCost

--- src/modules/aws/sns/sns.service.ts
+++ src/modules/aws/sns/sns.service.ts
@@ -36,6 +36,10 @@ export class SnsService {
   }
 
   async getCostAndUsage(start: string, end: string, granularity: "DAILY" | "HOURLY" | "MONTHLY", format: string): Promise<any> {
+    // end date should be latest date and start date should be 7 days before
+    end = new Date(end).toISOString();
+    start = new Date(new Date().setDate(new Date().getDate() - 7)).toISOString();
+    
     const params = {
       TimePeriod: {
         Start: start,
