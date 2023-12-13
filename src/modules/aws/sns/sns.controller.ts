84cdee3d00d1e530abc334711bd1f8f5b6c9de48
abidkhan03 <abidkhan42935@gmail.com>
2023-12-13 16:46:27 +0500

    Update getCost

--- src/modules/aws/sns/sns.controller.ts
+++ src/modules/aws/sns/sns.controller.ts
@@ -89,9 +89,10 @@ export class SnsController {
     ): Promise<any> {
         // end date should be latest date and start date should be 7 days before
         end = new Date().toISOString();
+        granularity = 'DAILY';
         start = new Date(new Date().setDate(new Date().getDate() - 7)).toISOString();
         this.logger.info(`granularity: ${granularity}`);
-        this.logger.info(`format: ${start}`);
+        this.logger.info(`format: ${format}`);
         this.logger.info(`Fetching cost data from ${start} to ${end} with granularity ${granularity}`);
         return this.snsService.getCostAndUsage(start, end, granularity, format);
     }
