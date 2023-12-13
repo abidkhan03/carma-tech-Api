7c340a217cb0e7dfafd4b9f611cc09f493ce77cc
abidkhan03 <abidkhan42935@gmail.com>
2023-12-14 00:14:12 +0500

    Filtered Dimension Services

--- src/modules/aws/services-cost/service-cost.service.ts
+++ src/modules/aws/services-cost/service-cost.service.ts
@@ -1,5 +1,5 @@
 import { Logger } from "@aws-lambda-powertools/logger";
-import { CostExplorerClient, GetCostAndUsageCommand, GroupDefinitionType } from "@aws-sdk/client-cost-explorer";
+import { CostExplorerClient, Dimension, GetCostAndUsageCommand, GroupDefinitionType } from "@aws-sdk/client-cost-explorer";
 import { Injectable } from "@nestjs/common";
 import { join } from "path";
 import * as fs from 'fs';
@@ -35,26 +35,38 @@ export class ServiceCostService {
             'UsageQuantity',
     
           ],
-          // Filter: {
-          //   Dimensions: {
-          //     Key: Dimension.SERVICE,
-          //     Values: [
-          //       'EC2: Running Hours',
-          //       'EC2: CloudWatch - Alarms',
-          //       'EC2: CloudWatch - Metrics',
-          //       'EC2: CloudWatch - Requests',
-          //       'EC2: Data Transfer - Inter AZ',
-          //       'EC2: Elastic IP - Additional Address',
-          //       'EC2: Elastic IP - Idle Address',
-          //       'EC2: NAT Gateway - Data Processed',
-          //       'EC2: NAT Gateway - Running Hours',
-          //       'RDS: Running Hours',
-          //       'RDS: I/O Requests',
-          //       'RDS: Storage',
-    
-          //     ]
-          //   }
-          // },
+          Filter: {
+            Dimensions: {
+              Key: Dimension.SERVICE,
+              Values: [
+                'AWS Lambda',
+                'AWS Step Functions',
+                'AWS Key Management Service',
+                'AWS Secrets Manager',
+                'Amazon EC2 Container Registry (ECR)',
+                'EC2 - Other',
+                'Amazon Relational Database Service',
+                'Amazon Simple Notification Service',
+                'Amazon Simple Storage Service',
+                'AmazonCloudWatch',
+                'Amazon Rekognition',
+                'AWS Glue',
+                'Amazon Athena',
+                'EC2: Running Hours',
+                'EC2: CloudWatch - Alarms',
+                'EC2: CloudWatch - Metrics',
+                'EC2: CloudWatch - Requests',
+                'EC2: Data Transfer - Inter AZ',
+                'EC2: Elastic IP - Additional Address',
+                'EC2: Elastic IP - Idle Address',
+                'EC2: NAT Gateway - Data Processed',
+                'EC2: NAT Gateway - Running Hours',
+                'RDS: Running Hours',
+                'RDS: I/O Requests',
+                'RDS: Storage',
+              ]
+            }
+          },
           GroupBy:[
             {
               Type: GroupDefinitionType.DIMENSION,
