f94ab5fd969a2883badbf11d44e3672b9590cc42
Abdullah Abid <69511228+abidkhan03@users.noreply.github.com>
2024-02-07 19:18:45 +0500

    Update auth.service.ts

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -19,8 +19,8 @@ export class AuthService {
     private readonly userService: UsersService,
   ) {
     this.userPool = new CognitoUserPool({
-      UserPoolId: 'us-east-2_0Pitx53J7',
-      ClientId: '5l1nf7orlu8lai7dpu83rs9551',
+      UserPoolId: this.configService.get('USER_POOL_ID') || 'us-east-2_0Pitx53J7',
+      ClientId: this.configService.get('COGNITO_USER_CLIENT_ID') || '5l1nf7orlu8lai7dpu83rs9551',
     });
   }
 
